db5d55c474595a45a596421811c0dec2a5410da2
Timo Hak <timo.hak@casema.nl>
2021-05-27 15:47:17 +0200

    laatste kleine aanpassingen, en bepaald welke unit tests ik ga gebruiken

---
+++ src/Unit_Tests/ShortestPath/ReisTest.java
@@ -0,0 +1,20 @@
+package ShortestPath;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReisTest {
+
+    @Test
+    public void getStappen() {
+    }
+
+    @Test
+    public void getZwaarte() {
+    }
+
+    @Test
+    public void compareTo() {
+    }
+}
---
+++ src/Unit_Tests/ShortestPath/StapTest.java
@@ -0,0 +1,12 @@
+package ShortestPath;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StapTest {
+
+    @Test
+    public void getZwaarte() {
+    }
+}
